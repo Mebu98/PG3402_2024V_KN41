af03f3dc2e6834e7995efd9fa5e0b3f253d28ab3
“Mebu98” <merijn.buitelaar98@gmail.com>
2024-02-28 23:58:27 +0100

    Frontend stuff + docker compose now also does frontend

---
+++ src/modules/frontend/src/functions/multiplicationFunctions.js
@@ -0,0 +1,25 @@
+export async function generateMultiplicationChallenge(userName, difficulty, challenge){
+    await fetch(`http://localhost:8080/api/v1/multiplication/generate/username/${userName}/difficulty/${difficulty}`,
+        {
+            method: 'GET'
+        }
+    ).then(response => response.json().then(data => {
+        challenge.value = data;
+    }))
+}
+
+export async function submitMultiplicationChallenge(challenge, userAnswer, submitResponse){
+    challenge.userAnswer = userAnswer;
+
+    await fetch(`http://localhost:8080/api/v1/multiplication/submit`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(challenge)
+        }
+    ).then(response => response.json().then(data => {
+        submitResponse.value = data;
+    }))
+}
